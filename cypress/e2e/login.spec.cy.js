328a192998ffe7d3b556fcd13bf6e73d868426c3
Tainha-Assada <otaviogomesm485@gmail.com>
2025-06-27 12:45:15 -0300

    first commit

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,18 @@
+describe('orange tests', () => {
+  it('success login', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+    cy.get("[type='submit']").click()
+    cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
+    cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  })
+
+  it('failed login', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin')
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type('admin12')
+    cy.get('.oxd-button').click()
+    cy.get("[role='alert']")
+  })
+})
